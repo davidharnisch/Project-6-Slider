337a4f497d2e5a7891082bf00cd31b5f4140807f
davidharnisch <davidharnisch@seznam.cz>
2021-11-26 23:05:14 +0100

    Final

--- js/app.js
+++ js/app.js
@@ -8,6 +8,33 @@
     "contBcg-3",
     "contBcg-4"
   ];
-d
+  
+  let btnLeft = document.querySelector(".btn-left")
+  let btnRight = document.querySelector(".btn-right")
+  let bgImage = document.querySelector(".img-container")
+let x = 0
+  
+
+
+  btnLeft.addEventListener("click", changeImageLeft);
+  btnRight.addEventListener("click", changeImageRight);
+
+  function changeImageLeft() {
+        
+    x--;
+    if (x<0) {
+      x = pictures.length - 1
+    }
+    bgImage.style.backgroundImage = "url('../img/" + pictures[x] + ".jpeg')";
+  }
+
+  function changeImageRight() {
+        
+    x++;
+    if (x>4) {
+      x = 0
+    }
+    bgImage.style.backgroundImage = "url('../img/" + pictures[x] + ".jpeg')";
+  }
  
 })();
